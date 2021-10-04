3ca167a64ebef50215f6dad99e5419f3c046f656
JackMBenson <jackmartinbenson@gmail.com>
2021-10-04 13:22:46 +1000

    cleaned up project and everything is one file now since the header files used were only pretty small

--- clObjects.h
+++ clObjects.h
@@ -3,15 +3,5 @@
 
 namespace
 {
-	struct clObjects
-	{
-		cl::Context context;
-		cl::Program program;
-		cl::Buffer inBuffer;
-		cl::Buffer outBuffer;
-		cl::BufferGL glBuffer;
-		cl::Kernel physicsKernel;
-		cl::Kernel renderKernel;
-		cl::CommandQueue queue;
-	};
+
 }
